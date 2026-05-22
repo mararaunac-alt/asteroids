16ba3a8c98bf1116868a8b05413de43ec3db705e
mararaunac-alt <mararaunac@gmail.com>
2026-05-22 16:39:19 -0600

    Asteroids dissapear when collide with a shot

--- main.py
+++ main.py
@@ -36,6 +36,12 @@ def main():
                 return
         updatable.update(dt)
         for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+        for asteroid in asteroids:
             if player.collides_with(asteroid):
                 log_event("player_hit")
                 print("Game over!")
