bc29b636f8e5a2109cd645d377e93a8c70409e29
mararaunac-alt <mararaunac@gmail.com>
2026-05-22 12:45:58 -0600

    Capping 60 FPS

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")
         display.flip() 
+        clock.tick(60)
+        dt = clock.tick(60) / 1000.0
 
 
 
